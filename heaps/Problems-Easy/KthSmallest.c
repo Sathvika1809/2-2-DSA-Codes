2fc4fea1a0436fc876cd2e81acabd60aa702cfb0
Sathvika <f20240192@hyderabad.bits-pilani.ac.in>
2026-02-21 01:22:42 +0530

    Done Quickselect

--- heaps/Problems-Easy/KthSmallest.c
+++ heaps/Problems-Easy/KthSmallest.c
@@ -33,13 +33,40 @@ int buildMaxHeap(int* arr,int n,int k){
     for(int i=(k/2)-1;i>=0;i--){
         heapify(arr,k,i);
     }
-    for(int i=k+1;i<n;i++){
+    for(int i=k;i<n;i++){
         if(arr[i]<arr[0]){
             arr[0]=arr[i];
             heapify(arr,k,0);
         }
     }
     return arr[0];
+}
+//Key is we want to Kth largest and Kth Smallest without sorting Fully
+//sorting ->O(nlogn) but QuickSelect-O(n)avg time
+int partition(int* arr,int l,int r){
+    int pivot = arr[r];
+    int i=l;
+    for(int j=l;j<r;j++){
+        if(arr[j]<=pivot){
+            swap(&arr[i],&arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i],&arr[r]);
+    return i;
+}
+int QuickSelect(int* arr,int l,int r,int k){
+    if(l<=r){
+        int pivotIdx = partition(arr,l,r);
+        if(pivotIdx == k) return arr[pivotIdx];
+        else if(pivotIdx > k) return QuickSelect(arr,l,pivotIdx-1,k);
+        //Kth largest means ntg convert idx to n-k instead of k
+        //2nd largest means 4th posn correct element
+        else return QuickSelect(arr,pivotIdx+1,r,k);
+        //We just look on the side where idx=k(sorted posn) on one side
+
+    }
+    return -1;
 }
 int main(){
     int n;
@@ -50,6 +77,18 @@ int main(){
     }
     int k;
     scanf("%d",&k);
-    printf("%d",buildMaxHeap(arr,n,k));
+    printf("%d\n",buildMaxHeap(arr,n,k));
+    int l=0,r=n-1;
+    int ans=QuickSelect(arr,l,r,n-k);//Kth largest
+    //2nd largest in n=6 means 4th smallest element
+    //For kth smallest call
+    //ans = Quickselect(arr,l,r,k-1);
+    
+    //Dont use heap and quickselect on one go u are using the changed array just wrote for
+    //referring other mthd
+    //Both are fine
+    printf("%d\n",ans);
     return 0;
 }
+//Using QuickSelect
+//To find Kth smallest Element,Kth LARGEST Element
